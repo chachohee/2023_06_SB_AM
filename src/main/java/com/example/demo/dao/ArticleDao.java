64d1ac2de0c1ad15fff2a59409757578a408e9ba
chachohee <cxoxee@gmail.com>
2023-06-19 21:17:44 +0900

    mybatis, mysql Driver, application.yml, mapper

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -1,69 +1,35 @@
 package com.example.demo.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.vo.Article;
 
-@Component
-public class ArticleDao {
+@Mapper
+public interface ArticleDao {
 	
-	private int lastArticleId;
-	private List<Article> articles;
+	public Article writeArticle(String title, String body);
 	
-	ArticleDao() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
-		makeTestData();
-	}
-	
-	public void makeTestData() {
-		for(int i = 1; i <= 10; i++) {
-			
-			String title = "제목" + i;
-			String body = "내용" + i;
-			
-			writeArticle(title, body);
-		}
-	}
+	@Select("SELECT * FROM article WHERE id = #{id}")
+	public Article getArticleById(int id);
 
-	public Article writeArticle(String title, String body) {
-		
-		int id = this.lastArticleId + 1;
-		this.lastArticleId = id;
-		
-		Article article = new Article(id, title, body);
-		
-		articles.add(article);
-		
-		return article;
-	}
+	@Update("""
+			UPDATE article 
+				SET updateDate = NOW()
+					, title = #{title}
+					, `body` = #{body} 
+				WHERE id = #{id}
+			""")
+	public void modifyArticle(int id, String title, String body);
 
-	public Article getArticleById(int id) {
-		
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(int id);
 
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticleById(id);
-		
-		article.setTitle(title); 
-		article.setBody(body);
-	}
-
-	public void deleteArticle(int id) {
-		Article article = getArticleById(id);
-		articles.remove(article);
-	}
-
-	public List<Article> getArticles() {
-		return this.articles;
-	}
+	@Select("SELECT * FROM article ORDER BY id DESC")
+	public List<Article> getArticles();
 }
